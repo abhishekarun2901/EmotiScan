326b82aee2c270aefe2227bf557e27ffa715bc2f
Abhijith26093 <abhijith26093@gmail.com>
2025-02-19 23:38:59 +0530

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,45 +1,29 @@
+from tensorflow.keras.models import model_from_json
+from tensorflow.python.keras.backend import set_session
+import numpy as np
+
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Data generators
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
 
-train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=30, zoom_range=0.2, shear_range=0.2, horizontal_flip=True)
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(48, 48),
-    batch_size=64,
-    color_mode='grayscale',
-    class_mode='categorical'
-)
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
-validation_generator = validation_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(48, 48),
-    batch_size=64,
-    color_mode='grayscale',
-    class_mode='categorical'
-)
+class FacialExpressionModel(object):
 
-# Build the model
-model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
-    MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Conv2D(128, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dropout(0.5),
-    Dense(7, activation='softmax')  # 7 emotion categories
-])
+    EMOTIONS_LIST = ["Angry", "Disgust",
+                     "Fear", "Happy",
+                     "Neutral", "Sad",
+                     "Surprise"]
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_generator, validation_data=validation_generator, epochs=25)
-model.save('emotion_model.h5')
+    def __init__(self, model_json_file, model_weights_file):
+        # load model from JSON file
+        with open(model_json_file, "r") as json_file:
+            loaded_model_json = json_file.read()
+            self.loaded_model = model_from_json(loaded_model_json)
 
+        # load weights into the new model
+        self.loaded_model.load_weights(model_weights_file)
+        #self.loaded_model.compile()
+        #self.loaded_model._make_predict_function()
+
+    def predict_emotion(self, img):
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
